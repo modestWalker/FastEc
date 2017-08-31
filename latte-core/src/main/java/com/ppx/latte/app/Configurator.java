4261f6becba7120d0a20da0579fc5aa9587f1cfb
PPX <756295457@qq.com>
2017-08-31 14:29:58 +0800

    init

---
+++ latte-core/src/main/java/com/ppx/latte/app/Configurator.java
@@ -0,0 +1,12 @@
+package com.ppx.latte.app;
+
+import java.util.WeakHashMap;
+
+/**
+ * 配置信息存储及获取
+ * Created by PPX on 2017/8/31.
+ */
+
+public class Configurator {
+//    private static final WeakHashMap<String,Object  >
+}
---
+++ latte-core/src/main/java/com/ppx/latte/app/Latte.java
@@ -0,0 +1,8 @@
+package com.ppx.latte.app;
+
+/**
+ * Created by PPX on 2017/8/31.
+ */
+
+public class Latte {
+}
